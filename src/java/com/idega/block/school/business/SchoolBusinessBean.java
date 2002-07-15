84d0ade6a9ef2a77ed658bfc67ee36125ebef2ba
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2002-07-15 12:56:05 +0000

    *** empty log message ***

--- src/java/com/idega/block/school/business/SchoolBusinessBean.java
+++ src/java/com/idega/block/school/business/SchoolBusinessBean.java
@@ -1,5 +1,4 @@
 package com.idega.block.school.business;
-
 import com.idega.block.school.data.*;
 import com.idega.data.IDOLookup;
 import java.util.Collection;
@@ -7,7 +6,11 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Iterator;
 import com.idega.business.IBOServiceBean;
-
+import com.idega.user.business.UserBusiness;
+import com.idega.user.data.User;
+import java.rmi.RemoteException;
+import javax.ejb.*;
+import com.idega.data.IDOCreateException;
 /**
  * <p>Title: </p>
  * <p>Description: </p>
@@ -16,165 +19,251 @@ import com.idega.business.IBOServiceBean;
  * @author <br><a href="mailto:aron@idega.is">Aron Birkir</a><br>
  * @version 1.0
  */
-
-public class SchoolBusinessBean extends IBOServiceBean implements SchoolBusiness{
-
-  public SchoolHome getSchoolHome(){
-    return getSchoolHome();
-  }
-  public School getSchool(Object primaryKey){
-     try{
-      SchoolHome shome = getSchoolHome();
-      return shome.findByPrimaryKey(primaryKey);
-    }
-    catch(Exception ex){
-      return null;
-    }
-  }
-
-  public void removeSchool(int id){
-    try{
-      School school = getSchool(new Integer(id));
-      school.remove();
-    }
-    catch(Exception ex){
-      ex.printStackTrace();
-
-    }
-  }
-
-  public Collection findAllSchools()  {
-    try{
-      SchoolHome shome = getSchoolHome();
-      return shome.findAllSchools();
-    }
-    catch(Exception ex){
-      ex.printStackTrace();
-      return new java.util.Vector();
-    }
-  }
-
-  public Collection findAllSchoolsByAreaAndType(int area,int type){
-    try{
-      SchoolHome shome = getSchoolHome();
-      return shome.findAllByAreaAndType(area,type);
-    }
-    catch(Exception ex){
-      ex.printStackTrace();
-      return new java.util.Vector();
-    }
-  }
-
-  public void storeSchool(
-              int id,
-              String name,
-              String info,
-              String address,
-              String zipcode,
-              String ziparea,
-              String phone,
-              String keycode,
-              String latitude,
-              String longitude,
-              int area_id,
-              int[] type_ids,
-              int[] year_ids
-      ) throws java.rmi.RemoteException{
-
-      SchoolHome shome = getSchoolHome();
-      School newSchool;
-      try{
-        if(id > 0){
-          newSchool = shome.findByPrimaryKey(new Integer(id));
-        }
-        else{
-          newSchool = shome.create();
-        }
-      }
-      catch(javax.ejb.FinderException fe){
-        throw new java.rmi.RemoteException(fe.getMessage());
-      }
-      catch(javax.ejb.CreateException ce){
-        throw new java.rmi.RemoteException(ce.getMessage());
-      }
-      newSchool.setSchoolAddress(address);
-      newSchool.setSchoolAreaId(area_id);
-      newSchool.setSchoolInfo(info);
-      newSchool.setSchoolKeyCode(keycode);
-      newSchool.setSchoolLatitude(latitude);
-      newSchool.setSchoolLongitude(longitude);
-      newSchool.setSchoolName(name);
-      newSchool.setSchoolPhone(phone);
-      newSchool.setSchoolZipArea(ziparea);
-      newSchool.setSchoolZipCode(zipcode);
-      newSchool.store();
-
-      if(type_ids!=null)
-        newSchool.addSchoolTypesRemoveOther(type_ids);
-      if(year_ids!=null)
-         newSchool.addSchoolYearsRemoveOther(year_ids);
-
-    }
-
-    public Map getSchoolRelatedSchoolTypes(School school){
-      try{
-        Collection types = school.findRelatedSchoolTypes();
-        if(types!=null && !types.isEmpty()){
-          HashMap map = new HashMap(types.size());
-          Iterator iter = types.iterator();
-          SchoolType type;
-          while(iter.hasNext()){
-            type = (SchoolType) iter.next();
-            map.put((Integer)type.getPrimaryKey(),type);
-          }
-          return map;
-        }
-      }
-      catch(Exception ex){
-        ex.printStackTrace();
-      }
-      return null;
-    }
-
-     public Map getSchoolRelatedSchoolYears(School school){
-      try{
-        Collection years = school.findRelatedSchoolYears();
-        if(years!=null && !years.isEmpty()){
-          HashMap map = new HashMap(years.size());
-          Iterator iter = years.iterator();
-          SchoolYear year;
-          while(iter.hasNext()){
-            year = (SchoolYear) iter.next();
-            map.put(year.getPrimaryKey(),year);
-          }
-          return map;
-        }
-      }
-      catch(Exception ex){
-        ex.printStackTrace();
-      }
-      return null;
-    }
-
-     public Map getMapOfSchools(){
-      try{
-        Collection schools = findAllSchools();
-        if(schools!=null && !schools.isEmpty()){
-          HashMap map = new HashMap(schools.size());
-          Iterator iter = schools.iterator();
-          School school;
-          while(iter.hasNext()){
-            school = (School) iter.next();
-            map.put(school.getPrimaryKey(),school);
-          }
-          return map;
-        }
-      }
-      catch(Exception ex){
-        ex.printStackTrace();
-      }
-      return null;
-    }
-
-
+public class SchoolBusinessBean extends IBOServiceBean implements SchoolBusiness
+{
+	public SchoolHome getSchoolHome()
+	{
+		return getSchoolHome();
+	}
+	public School getSchool(Object primaryKey)
+	{
+		try
+		{
+			SchoolHome shome = getSchoolHome();
+			return shome.findByPrimaryKey(primaryKey);
+		}
+		catch (Exception ex)
+		{
+			return null;
+		}
+	}
+	public void removeSchool(int id)
+	{
+		try
+		{
+			School school = getSchool(new Integer(id));
+			school.remove();
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	public Collection findAllSchools()
+	{
+		try
+		{
+			SchoolHome shome = getSchoolHome();
+			return shome.findAllSchools();
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			return new java.util.Vector();
+		}
+	}
+	public Collection findAllSchoolsByAreaAndType(int area, int type)
+	{
+		try
+		{
+			SchoolHome shome = getSchoolHome();
+			return shome.findAllByAreaAndType(area, type);
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			return new java.util.Vector();
+		}
+	}
+	public School createSchool(
+		String name,
+		String info,
+		String address,
+		String zipcode,
+		String ziparea,
+		String phone,
+		String keycode,
+		String latitude,
+		String longitude,
+		int area_id,
+		int[] type_ids)
+		throws CreateException
+	{
+		return createSchool(
+			name,
+			info,
+			address,
+			zipcode,
+			ziparea,
+			phone,
+			keycode,
+			latitude,
+			longitude,
+			area_id,
+			type_ids,
+			null);
+	}
+	public School createSchool(
+		String name,
+		String info,
+		String address,
+		String zipcode,
+		String ziparea,
+		String phone,
+		String keycode,
+		String latitude,
+		String longitude,
+		int area_id,
+		int[] type_ids,
+		int[] year_ids)
+		throws CreateException
+	{
+		try
+		{
+			return storeSchool(
+				-1,
+				name,
+				info,
+				address,
+				zipcode,
+				ziparea,
+				phone,
+				keycode,
+				latitude,
+				longitude,
+				area_id,
+				type_ids,
+				year_ids);
+		}
+		catch (RemoteException rme)
+		{
+			throw new IDOCreateException(rme);
+		}
+	}
+	public School storeSchool(
+		int id,
+		String name,
+		String info,
+		String address,
+		String zipcode,
+		String ziparea,
+		String phone,
+		String keycode,
+		String latitude,
+		String longitude,
+		int area_id,
+		int[] type_ids,
+		int[] year_ids)
+		throws java.rmi.RemoteException
+	{
+		SchoolHome shome = getSchoolHome();
+		School newSchool;
+		try
+		{
+			if (id > 0)
+			{
+				newSchool = shome.findByPrimaryKey(new Integer(id));
+			}
+			else
+			{
+				newSchool = shome.create();
+			}
+		}
+		catch (javax.ejb.FinderException fe)
+		{
+			throw new java.rmi.RemoteException(fe.getMessage());
+		}
+		catch (javax.ejb.CreateException ce)
+		{
+			throw new java.rmi.RemoteException(ce.getMessage());
+		}
+		newSchool.setSchoolAddress(address);
+		newSchool.setSchoolAreaId(area_id);
+		newSchool.setSchoolInfo(info);
+		newSchool.setSchoolKeyCode(keycode);
+		newSchool.setSchoolLatitude(latitude);
+		newSchool.setSchoolLongitude(longitude);
+		newSchool.setSchoolName(name);
+		newSchool.setSchoolPhone(phone);
+		newSchool.setSchoolZipArea(ziparea);
+		newSchool.setSchoolZipCode(zipcode);
+		newSchool.store();
+		if (type_ids != null)
+			newSchool.addSchoolTypesRemoveOther(type_ids);
+		if (year_ids != null)
+			newSchool.addSchoolYearsRemoveOther(year_ids);
+		return newSchool;
+	}
+	public Map getSchoolRelatedSchoolTypes(School school)
+	{
+		try
+		{
+			Collection types = school.findRelatedSchoolTypes();
+			if (types != null && !types.isEmpty())
+			{
+				HashMap map = new HashMap(types.size());
+				Iterator iter = types.iterator();
+				SchoolType type;
+				while (iter.hasNext())
+				{
+					type = (SchoolType) iter.next();
+					map.put((Integer) type.getPrimaryKey(), type);
+				}
+				return map;
+			}
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	public Map getSchoolRelatedSchoolYears(School school)
+	{
+		try
+		{
+			Collection years = school.findRelatedSchoolYears();
+			if (years != null && !years.isEmpty())
+			{
+				HashMap map = new HashMap(years.size());
+				Iterator iter = years.iterator();
+				SchoolYear year;
+				while (iter.hasNext())
+				{
+					year = (SchoolYear) iter.next();
+					map.put(year.getPrimaryKey(), year);
+				}
+				return map;
+			}
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	public Map getMapOfSchools()
+	{
+		try
+		{
+			Collection schools = findAllSchools();
+			if (schools != null && !schools.isEmpty())
+			{
+				HashMap map = new HashMap(schools.size());
+				Iterator iter = schools.iterator();
+				School school;
+				while (iter.hasNext())
+				{
+					school = (School) iter.next();
+					map.put(school.getPrimaryKey(), school);
+				}
+				return map;
+			}
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return null;
+	}
 }
