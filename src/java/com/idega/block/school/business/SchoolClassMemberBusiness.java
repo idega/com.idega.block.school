b02145af4c3ea8ba034fefc4d332d0e0c6c021d7
Thorhallur Helgason <laddi@idega.com>
2002-10-16 09:58:14 +0000

    *** empty log message ***

--- src/java/com/idega/block/school/business/SchoolClassMemberBusiness.java
+++ src/java/com/idega/block/school/business/SchoolClassMemberBusiness.java
@@ -4,11 +4,11 @@ import javax.ejb.*;
 
 public interface SchoolClassMemberBusiness extends com.idega.business.IBOService,com.idega.block.process.business.CaseBusiness
 {
- public java.util.Collection findClassMember(int p0)throws javax.ejb.FinderException,java.rmi.RemoteException, java.rmi.RemoteException;
- public com.idega.block.school.data.SchoolClassMember findClassMemberInClass(int p0,int p1)throws javax.ejb.FinderException,java.rmi.RemoteException, java.rmi.RemoteException;
- public java.util.Collection findStudentsInClass(int p0)throws javax.ejb.FinderException,java.rmi.RemoteException, java.rmi.RemoteException;
+ public java.util.Collection findClassMember(int p0)throws java.rmi.RemoteException, java.rmi.RemoteException;
+ public com.idega.block.school.data.SchoolClassMember findClassMemberInClass(int p0,int p1)throws java.rmi.RemoteException, java.rmi.RemoteException;
+ public java.util.Collection findStudentsInClass(int p0)throws java.rmi.RemoteException, java.rmi.RemoteException;
  public com.idega.block.school.data.SchoolClassMemberHome getSchoolClassMemberHome()throws java.rmi.RemoteException, java.rmi.RemoteException;
- public void removeSchoolClassMember(int p0)throws javax.ejb.FinderException,java.rmi.RemoteException,javax.ejb.RemoveException, java.rmi.RemoteException;
- public void removeSchoolClassMemberFromClass(int p0,int p1)throws javax.ejb.FinderException,javax.ejb.RemoveException,java.rmi.RemoteException, java.rmi.RemoteException;
- public void storeSchoolClassMember(int p0,int p1,java.sql.Timestamp p2,int p3)throws javax.ejb.CreateException,javax.ejb.FinderException,java.rmi.RemoteException, java.rmi.RemoteException;
+ public void removeSchoolClassMember(int p0)throws java.rmi.RemoteException, java.rmi.RemoteException;
+ public void removeSchoolClassMemberFromClass(int p0,int p1)throws java.rmi.RemoteException, java.rmi.RemoteException;
+ public void storeSchoolClassMember(int p0,int p1,java.sql.Timestamp p2,int p3)throws java.rmi.RemoteException, java.rmi.RemoteException;
 }
--- src/java/com/idega/block/school/business/SchoolClassMemberBusinessBean.java
+++ src/java/com/idega/block/school/business/SchoolClassMemberBusinessBean.java
@@ -4,6 +4,7 @@ import java.rmi.RemoteException;
 import java.sql.Timestamp;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Vector;
 
 import javax.ejb.CreateException;
 import javax.ejb.FinderException;
@@ -29,49 +30,87 @@ public class SchoolClassMemberBusinessBean extends CaseBusinessBean implements S
 		return (SchoolClassMemberHome) IDOLookup.getHome(SchoolClassMember.class);
 	}
 
-	public SchoolClassMember findClassMemberInClass(int studentID, int schoolClassID) throws FinderException, RemoteException {
-		return getSchoolClassMemberHome().findByUserAndSchoolClass(studentID, schoolClassID);
+	public SchoolClassMember findClassMemberInClass(int studentID, int schoolClassID) throws RemoteException {
+		try {
+			return getSchoolClassMemberHome().findByUserAndSchoolClass(studentID, schoolClassID);
+		}
+		catch (FinderException fe) {
+			fe.printStackTrace(System.err);
+			return null;
+		}
 	}
 
-	public Collection findClassMember(int studentID) throws FinderException, RemoteException {
-		return getSchoolClassMemberHome().findByStudent(studentID);
+	public Collection findClassMember(int studentID) throws RemoteException {
+		try {
+			return getSchoolClassMemberHome().findByStudent(studentID);
+		}
+		catch (FinderException fe) {
+			fe.printStackTrace(System.err);
+			return new Vector();
+		}
 	}
 	
-	public Collection findStudentsInClass(int studentClassID) throws FinderException, RemoteException {
-		return getSchoolClassMemberHome().findBySchoolClass(studentClassID);	
+	public Collection findStudentsInClass(int studentClassID) throws RemoteException {
+		try {
+			return getSchoolClassMemberHome().findBySchoolClass(studentClassID);	
+		}
+		catch (FinderException fe) {
+			fe.printStackTrace(System.err);
+			return new Vector();
+		}
 	}
 
-	public void removeSchoolClassMemberFromClass(int studentID, int schoolClassID) throws FinderException, RemoveException, RemoteException {
-		SchoolClassMember member = findClassMemberInClass(studentID, schoolClassID);
-		member.remove();
+	public void removeSchoolClassMemberFromClass(int studentID, int schoolClassID) throws RemoteException {
+		try {
+			SchoolClassMember member = getSchoolClassMemberHome().findByUserAndSchoolClass(studentID, schoolClassID);
+			member.remove();
+		}
+		catch (FinderException fe) {
+			fe.printStackTrace(System.err);
+		}
+		catch (RemoveException re) {
+			re.printStackTrace(System.err);
+		}
 	}
 	
-	public void removeSchoolClassMember(int studentID) throws FinderException, RemoteException, RemoveException {
-		Collection members = findClassMember(studentID);
-		if ( members != null ) {
-			Iterator iter = members.iterator();
-			while (iter.hasNext()) {
-				SchoolClassMember member = (SchoolClassMember) iter.next();
-				member.remove();
+	public void removeSchoolClassMember(int studentID) throws RemoteException {
+		try {
+			Collection members = findClassMember(studentID);
+			if ( !members.isEmpty() ) {
+				Iterator iter = members.iterator();
+				while (iter.hasNext()) {
+					SchoolClassMember member = (SchoolClassMember) iter.next();
+					member.remove();
+				}	
 			}	
-		}	
+		}
+		catch (RemoveException re) {
+			re.printStackTrace(System.err);
+		}
 	}
 	
-	public void storeSchoolClassMember(int studentID,int schoolClassID,Timestamp registerDate,int registrator) throws CreateException, FinderException, RemoteException {
-		SchoolClassMember member;
-		if ( schoolClassID != -1 )
-			member = this.findClassMemberInClass(studentID,schoolClassID);
-		else
-			member = this.getSchoolClassMemberHome().create();
-			
-		member.setClassMemberId(studentID);
-		member.setSchoolClassId(schoolClassID);
-		if ( registerDate != null )
-			member.setRegisterDate(registerDate);
-		if ( registrator != -1 )
-			member.setRegistratorId(registrator);
-		
-		member.store();
+	public void storeSchoolClassMember(int studentID,int schoolClassID,Timestamp registerDate,int registrator) throws RemoteException {
+		try {
+			SchoolClassMember member;
+			if ( schoolClassID != -1 )
+				member = this.findClassMemberInClass(studentID,schoolClassID);
+			else
+				member = this.getSchoolClassMemberHome().create();
+				
+			if (member != null ) {
+				member.setClassMemberId(studentID);
+				member.setSchoolClassId(schoolClassID);
+				if ( registerDate != null )
+					member.setRegisterDate(registerDate);
+				if ( registrator != -1 )
+					member.setRegistratorId(registrator);
+				
+				member.store();
+			}
+		}
+		catch (CreateException ce) {
+			ce.printStackTrace(System.err);	
+		}	
 	}
 
 }
