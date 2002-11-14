f39a9af2ff119283a01dc9f6c59a4f06d1e69ae4
Grimur Jonsson <gimmi@idega.com>
2002-11-14 22:45:52 +0000

    Changed text, to imageButton

--- src/java/com/idega/block/school/presentation/SchoolContentItemEditButton.java
+++ src/java/com/idega/block/school/presentation/SchoolContentItemEditButton.java
@@ -4,6 +4,8 @@ import java.rmi.RemoteException;
 
 import com.idega.presentation.PresentationObject;
 import com.idega.presentation.text.Link;
+import com.idega.presentation.ui.Form;
+import com.idega.presentation.ui.SubmitButton;
 
 /**
  * @author gimmi
@@ -18,7 +20,8 @@ public class SchoolContentItemEditButton extends SchoolContentItem {
 //		System.out.println("SchoolContentItemEditButton : hasContentEdit = "+super.getSchoolContentBusiness(_iwc).hasEditPermission(_school, super._iwc));
 		
 		
-		Link link = SchoolContentEditor.getLink(_school);
+		Link link = SchoolContentEditor.getLink(_school, _iwrb.getLocalizedImageButton("content_editor","Content Editor"));
+
 		if (super.hasEditPermission()) {
 			return link;
 		}else if (super.getSchoolBusiness(_iwc).hasEditPermission(_iwc.getCurrentUser(), _school)) {
