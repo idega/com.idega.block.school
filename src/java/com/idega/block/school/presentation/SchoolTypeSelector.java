6f63762aaac993ced4bf9df598d953c87545860f
Thorhallur Helgason <laddi@idega.com>
2002-12-19 16:12:44 +0000

    Changed layout and added method to display only school types in a specific category.

--- src/java/com/idega/block/school/presentation/SchoolTypeSelector.java
+++ src/java/com/idega/block/school/presentation/SchoolTypeSelector.java
@@ -22,9 +22,9 @@ import com.idega.presentation.text.Text;
  */
 
 public class SchoolTypeSelector extends Block {
-  public static final String IW_BUNDLE_IDENTIFIER = "com.idega.block.school";
-  
- 	public static String PARAMETER_SCHOOL_TYPE_ID = "schsel_schtyid";
+	public static final String IW_BUNDLE_IDENTIFIER = "com.idega.block.school";
+
+	public static String PARAMETER_SCHOOL_TYPE_ID = "schsel_schtyid";
 	private int _schoolTypeId = -1;
 
 	private boolean _horizontal = true;
@@ -32,12 +32,12 @@ public class SchoolTypeSelector extends Block {
 	private String _fontColor;
 	private String _selStyle;
 	private String _selColor;
-	private String _spaceBetween = "5";
+	private int _spaceBetween = 5;
+	private String _category;
 
-
-	public void main(IWContext iwc) throws RemoteException{
+	public void main(IWContext iwc) throws RemoteException {
 		init(iwc);
-		
+
 		drawList(iwc);
 	}
 
@@ -45,19 +45,26 @@ public class SchoolTypeSelector extends Block {
 		if (iwc.isParameterSet(PARAMETER_SCHOOL_TYPE_ID)) {
 			try {
 				_schoolTypeId = Integer.parseInt(iwc.getParameter(PARAMETER_SCHOOL_TYPE_ID));
-			}catch (NumberFormatException n) {
+			}
+			catch (NumberFormatException n) {
 				n.printStackTrace(System.err);
-			}	
+			}
 		}
 	}
 
 	public String getBundleIdentifier() {
-			return IW_BUNDLE_IDENTIFIER;
-	}	
+		return IW_BUNDLE_IDENTIFIER;
+	}
 	private void drawList(IWContext iwc) throws RemoteException {
-		SchoolBusiness stb = (SchoolBusiness) IBOLookup.getServiceInstance( iwc, SchoolBusiness.class);
-		Collection coll = stb.findAllSchoolTypes();
-		
+		SchoolBusiness stb = (SchoolBusiness) IBOLookup.getServiceInstance(iwc, SchoolBusiness.class);
+		Collection coll;
+		if (_category != null) {
+			coll = stb.findAllSchoolTypesInCategory(_category);
+		}
+		else {
+			coll = stb.findAllSchoolTypes();
+		}
+
 		if (coll != null) {
 			SchoolType sType;
 			SchoolTypeHome stHome = (SchoolTypeHome) IDOLookup.getHome(SchoolType.class);
@@ -67,94 +74,101 @@ public class SchoolTypeSelector extends Block {
 			Link link;
 			int row = 1;
 			int col = 1;
-			if (_horizontal) {
-				col = 0;		
-			}else {
-				row = 0;	
-			}
-			
-			Iterator iter = coll.iterator();	
+
+			Iterator iter = coll.iterator();
 			while (iter.hasNext()) {
+				sType = (SchoolType) iter.next();
+				sTypeId = ((Integer) sType.getPrimaryKey()).intValue();
+				
+				if (sTypeId == _schoolTypeId) {
+					table.add(getText(sType.getName(), true), col, row);
+				}
+				else {
+					table.add(getLink(sType.getName(), sType.getPrimaryKey().toString()), col, row);
+				}
+				
+				if (_spaceBetween > 0 && iter.hasNext()) {
 					if (_horizontal) {
-						++col;		
-						table.setWidth(col, row, _spaceBetween);
-						++col;
-					}else {
-						++row;	
-						table.setHeight(col, row, _spaceBetween);
-						++row;
+						table.setWidth(++col, row, String.valueOf(_spaceBetween));
 					}
-					
-					sType = (SchoolType) iter.next() ;
-					sTypeId = ((Integer) sType.getPrimaryKey()).intValue();
-					if (sTypeId == _schoolTypeId) {
-						table.add(getText(sType.getName(), true), col, row);	
-					}else {
-						table.add(getLink(sType.getName(), sType.getPrimaryKey().toString() ), col, row);
+					else {
+						table.setHeight(col, ++row, String.valueOf(_spaceBetween));	
 					}
+				}
+
+				if (_horizontal) {
+					col++;		
+				}
+				else {
+					row++;	
+				}
 			}
 			add(table);
-		}else {
-			add("No types found");	
 		}
-			
+		else {
+			add("No types found");
+		}
+
 	}
-	
+
 	private Link getLink(String content, String primaryKey) {
 		Link link = new Link(getText(content, false));
 		link.addParameter(PARAMETER_SCHOOL_TYPE_ID, primaryKey);
 		return link;
 	}
-	
+
 	private Text getText(String content, boolean selected) {
 		Text text = new Text(content);
 		if (selected) {
 			if (_selColor != null) {
 				text.setFontColor(_selColor);
-			}	
-			if (_selStyle != null) {
-				text.setFontStyle(_selStyle);	
 			}
-		}else {
+			if (_selStyle != null) {
+				text.setFontStyle(_selStyle);
+			}
+		}
+		else {
 			if (_fontColor != null) {
 				text.setFontColor(_fontColor);
-			}	
+			}
 			if (_fontStyle != null) {
-				text.setFontStyle(_fontStyle);	
+				text.setFontStyle(_fontStyle);
 			}
 		}
 		return text;
 	}
-	
-	
+
 	/** Setters */
-	
+
 	public void setHorizontalView(boolean horizontal) {
-		_horizontal = horizontal;	
+		_horizontal = horizontal;
 	}
-	
+
 	public void setVerticalView(boolean vertical) {
-		_horizontal = !vertical;	
+		_horizontal = !vertical;
 	}
-	
+
 	public void setSpaceBetween(int spaceBetween) {
-		_spaceBetween = Integer.toString( spaceBetween) ;
+		_spaceBetween = spaceBetween;
 	}
-	
+
 	public void setFontStyle(String style) {
-		_fontStyle = style;	
+		_fontStyle = style;
 	}
-	
+
 	public void setFontColor(String color) {
-		_fontColor = color;	
+		_fontColor = color;
 	}
-	
+
 	public void setSelectedFontStyle(String style) {
-		_selStyle = style;	
+		_selStyle = style;
 	}
-	
+
 	public void setSelectedFontColor(String color) {
-		_selColor	= color;
+		_selColor = color;
 	}
-	
+
+	public void setSchoolCategory(String category) {
+		_category = category;
+	}
 }
