611d43da7d762856166ded323e265a004307f147
Grimur Jonsson <gimmi@idega.com>
2002-12-26 17:20:46 +0000

    added getHome()

--- src/java/com/idega/block/school/data/SchoolUser.java
+++ src/java/com/idega/block/school/data/SchoolUser.java
@@ -9,6 +9,7 @@ public interface SchoolUser extends com.idega.data.IDOEntity
  public void setSchoolId(int p0);
  public void initializeAttributes();
  public int getSchoolId();
+ public com.idega.block.school.data.SchoolUserHome getHome()throws java.rmi.RemoteException;
  public void setUserType(int p0);
  public int getUserType();
 }
