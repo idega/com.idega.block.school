e6e4fa4b32f2e4b76d15d6a08b5552d392e4905b
Grimur Jonsson <gimmi@idega.com>
2003-01-16 14:39:14 +0000

    Added getSchoolUserTypes method

--- src/java/com/idega/block/school/business/SchoolUserBusinessBean.java
+++ src/java/com/idega/block/school/business/SchoolUserBusinessBean.java
@@ -11,12 +11,15 @@ import javax.ejb.RemoveException;
 
 import com.idega.block.school.data.School;
 import com.idega.block.school.data.SchoolHome;
+import com.idega.block.school.data.SchoolType;
+import com.idega.block.school.data.SchoolTypeHome;
 import com.idega.block.school.data.SchoolUser;
 import com.idega.block.school.data.SchoolUserBMPBean;
 import com.idega.block.school.data.SchoolUserHome;
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOServiceBean;
 import com.idega.data.IDOLookup;
+import com.idega.data.IDORelationshipException;
 import com.idega.user.business.UserBusiness;
 import com.idega.user.data.User;
 import com.idega.user.data.UserHome;
@@ -53,13 +56,11 @@ public class SchoolUserBusinessBean extends IBOServiceBean implements SchoolUser
 	}
 
 	public SchoolUser addAssistantHeadmaster(School school, User user) throws RemoteException, CreateException, FinderException {
-		SchoolUser sUser = addUser(school, user, USER_TYPE_ASSISTANT_HEADMASTER);	
-		return sUser;
+		return addUser(school, user, USER_TYPE_ASSISTANT_HEADMASTER);	
 	}
 
 	public SchoolUser addWebAdmin(School school, User user) throws RemoteException, CreateException, FinderException {
-		SchoolUser sUser = addUser(school, user, USER_TYPE_WEB_ADMIN);	
-		return sUser;
+		return addUser(school, user, USER_TYPE_WEB_ADMIN);	
 	}
 
 	public void setUserGroups(School school, User user, int userType) throws RemoteException, FinderException {
@@ -101,110 +102,24 @@ public class SchoolUserBusinessBean extends IBOServiceBean implements SchoolUser
 	}
 	
 	public Collection getHeadmasters(School school) throws RemoteException, FinderException{
-		Collection coll = getSchoolUserHome().findBySchoolAndType(school, USER_TYPE_HEADMASTER);	
-/*
-		// Backwards compatability 
-		if ( coll == null || coll.size() < 1 ) {
-			int headmasterId = school.getHeadmasterUserId();
-			if (headmasterId > 0) {
-				try {
-					User user = ((UserHome) IDOLookup.getHome(User.class)).findByPrimaryKey(new Integer(headmasterId));
-					coll = new Vector();
-					coll.add(user.getPrimaryKey());
-				
-					addHeadmaster(school, user);
-					System.out.println("[SchoolUserBusinessBean] : Backwards compatability for school headmasters");
-				} catch (CreateException e) {
-					System.out.println("[SchoolUserBusinessBean] : Backwards compatability for school headmasters failed (addHeadmaster())");
-				} catch (FinderException e) {
-					System.out.println("[SchoolUserBusinessBean] : Backwards compatability for school headmasters failed cannot find hm with ID = "+headmasterId);
-				}
-			}
-		}
-*/
-		return coll;
+		return getSchoolUserHome().findBySchoolAndType(school, USER_TYPE_HEADMASTER);	
 	}
 
 	public Collection getAssistantHeadmasters(School school) throws RemoteException, FinderException{
-		Collection coll = getSchoolUserHome().findBySchoolAndType(school, USER_TYPE_ASSISTANT_HEADMASTER);	
-/*
-		// Backwards compatability 
-		if ( coll == null || coll.size() < 1 ) {
-			int ahgi = school.getAssistantHeadmasterGroupId();
-			if (ahgi > 0) {
-				try {
-					Collection users = getUserBusiness().getGroupBusiness().getUsers(ahgi);
-					if ( users != null && users.size() > 0 ) {
-						User user;
-						Iterator iter = users.iterator();
-						coll = new Vector();
-						while (iter.hasNext()) {
-							try {
-								user = (User) iter.next();
-								coll.add(user.getPrimaryKey());
-								addWebAdmin(school, user);
-								System.out.println("[SchoolUserBusinessBean] : Backwards compatability for school assistant headmasters");
-							} catch (CreateException e) {
-								System.out.println("[SchoolUserBusinessBean] : Backwards compatability for school assistant headmasters failed (addWebAdmin())");
-		//					e.printStackTrace(System.err);
-							} catch (FinderException e) {
-								System.out.println("[SchoolUserBusinessBean] : Backwards compatability for school assistant headmasters failed, cannot find user with ID "+ahgi);
-							}
-						}
-						
-					}
-				}catch (Exception fe) {
-					System.out.println("[SchoolUserBusinessBean] : getAssistantHeadmasters() : cannot find any users...");
-				}
-			}
-		}
-*/
-		return coll;
+		return getSchoolUserHome().findBySchoolAndType(school, USER_TYPE_ASSISTANT_HEADMASTER);	
 	}
 	
 	public Collection getWebAdmins(School school) throws RemoteException, FinderException{
-		Collection coll = getSchoolUserHome().findBySchoolAndType(school, USER_TYPE_WEB_ADMIN);	
-/*		
-		// backwards compatability 
-		if ( coll == null || coll.size() < 1 ) {
-			int hgi = school.getHeadmasterGroupId();
-			if (hgi > 0) {
-				try {
-					Collection users = getUserBusiness().getGroupBusiness().getUsers(hgi);
-					if ( users != null && users.size() > 0 ) {
-						User user;
-						Iterator iter = users.iterator();
-						coll = new Vector();
-						while (iter.hasNext()) {
-							try {
-								user = (User) iter.next();
-								coll.add(user.getPrimaryKey());
-								addWebAdmin(school, user);
-								System.out.println("[SchoolUserBusinessBean] : Backwards compatability for school web admins");
-							} catch (CreateException e) {
-								System.out.println("[SchoolUserBusinessBean] : Backwards compatability for school web admins failed (addWebAdmin())");
-		//					e.printStackTrace(System.err);
-							} catch (FinderException e) {
-								System.out.println("[SchoolUserBusinessBean] : Backwards compatability for school web admins failed, cannot find user with Id = "+hgi);
-		//					e.printStackTrace(System.err);
-							}
-						}
-						
-					}
-				}catch (Exception fe) {
-					System.out.println("[SchoolUserBusinessBean] : getWebAdmins() : cannot find any users...");
-				}
-			}
-		}
-*/		
-		return coll;
+		return getSchoolUserHome().findBySchoolAndType(school, USER_TYPE_WEB_ADMIN);	
+	}
+
+	public Collection getUsers(School school, int userType) throws RemoteException, FinderException {
+		return getSchoolUserHome().findBySchoolAndType(school, userType);	
 	}
 
 	public Collection getSchools(User user) throws RemoteException, FinderException {
-//		System.out.println("[SchoolUserBusinessBean] : halo eg heitir recursive");
 		Collection csUser = getSchoolUserHome().findByUser(user);
 		if (csUser != null || !csUser.isEmpty()) {
-//			System.out.println("[SchoolUserBusinessBean] : found school(s)....");
 			Collection coll = new Vector();
 			Iterator iter = csUser.iterator();
 			SchoolUser sUser;
@@ -214,38 +129,90 @@ public class SchoolUserBusinessBean extends IBOServiceBean implements SchoolUser
 			}
 			return coll;
 		}
-		/*else {
-//			System.out.println("[SchoolUserBusinessBean] : trying to backwards.....");
-			Collection schools =
-				((SchoolBusiness) IBOLookup.getServiceInstance(this.getIWApplicationContext(), SchoolBusiness.class))
-					.getSchoolHome()
-					.findAllBySchoolGroup(user);
-			if (schools != null && !schools.isEmpty()) {
-//				System.out.println("[SchoolUserBusinessBean] : trying to backwards .. school != null and not empty");
-				School school;
-				Iterator iter = schools.iterator();
-				while (iter.hasNext()) {
-					school = getSchoolHome().findByPrimaryKey(iter.next());
-					try {
-						addWebAdmin(school, user);
-						System.out.println("[SchoolUserBusinessBean] : Backwards compatability for schools ...");
-					} catch (Exception e) {
-						System.out.println("[SchoolUserBusinessBean] : Backwards compatability for schools FAILED");
-					}	
-				}
-			}else {
-//				System.out.println("[SchoolUserBusinessBean] : trying to backwards .. school == null or empty");
-			}		
-			
-			return getSchools(user);
-			
-		}*/
+
 		return null;
+	}
+	
+	/**
+	 *	Returns a collection of Strings. "SCHOOL" or "CHILDCARE" or both 
+	 * 
+	 */
+	public Collection getSchoolTypeCategories(School school) throws IDORelationshipException, RemoteException, FinderException {
+		Collection sTypes = school.getSchoolTypes();
+		SchoolType sType;
+		boolean SCHOOL = false;
+		boolean CHILDCARE = false;
+		
+		if (sTypes != null && !sTypes.isEmpty() ) {
+			Iterator iter = sTypes.iterator();
+			String sCat;
+			while (iter.hasNext()) {
+				sType = getSchoolTypeHome().findByPrimaryKey(iter.next());
+				sCat = sType.getSchoolCategory();
+				if (sCat != null && sCat.equals("SCHOOL")){
+					SCHOOL = true;
+				}else if (sCat != null && sCat.equals("CHILDCARE")) {
+					CHILDCARE = true;
+				}
+			}
+		}
+		
+		Collection coll = new Vector();
+		if (SCHOOL) {
+			coll.add("SCHOOL");	
+		}
+		if (CHILDCARE) {
+			coll.add("CHILDCARE");	
+		}
+		
+		return coll;
+	}
+	/**
+	 * Returns a collection of String[], [0] = localization key for schoolUserType
+	 * [1] = default value if localization in null  [2] = userTypeId.
+	 * @param school
+	 * @return Collection
+	 * @throws IDORelationshipException
+	 * @throws RemoteException
+	 * @throws FinderException
+	 */
+	public Collection getSchoolUserTypes(School school) throws IDORelationshipException, RemoteException, FinderException {
+		Collection schoolTypeCategories = getSchoolTypeCategories(school);
+		Collection userTypes = null;
+		if (schoolTypeCategories != null && !schoolTypeCategories.isEmpty()) {
+			userTypes = new Vector();
+			String category = "";
+			if (schoolTypeCategories.size() == 1){
+				Iterator iter = schoolTypeCategories.iterator();
+				while (iter.hasNext()) {
+					category = (String) iter.next();
+				}	
+			}else {
+				category = "BOTH";
+			}
+
+			if (category.equals("SCHOOL") || category.equals("BOTH")) {
+				userTypes.add(new String[] {"school.headmaster", "Headmaster", Integer.toString(USER_TYPE_HEADMASTER) });
+				userTypes.add(new String[] {"school.assistant_headmaster", "Assistant headmaster", Integer.toString(USER_TYPE_ASSISTANT_HEADMASTER) });
+				userTypes.add(new String[] {"school.web_administrators", "Web administrators", Integer.toString(USER_TYPE_WEB_ADMIN) });
+				userTypes.add(new String[] {"school.teachers", "Teachers", Integer.toString(USER_TYPE_TEACHER) });
+			}else if (category.equals("CHILDCARE")) {
+				userTypes.add(new String[] {"school.manager", "Manager", Integer.toString(USER_TYPE_HEADMASTER) });
+				userTypes.add(new String[] {"school.web_administrators", "Web administrators", Integer.toString(USER_TYPE_WEB_ADMIN) });
+				userTypes.add(new String[] {"school.teachers", "Teachers", Integer.toString(USER_TYPE_TEACHER) });
+			}
+			
+		}
+		return userTypes;	
 	}
 
 
 	private UserBusiness getUserBusiness() throws RemoteException {
 		return  (UserBusiness) IBOLookup.getServiceInstance(getIWApplicationContext(), UserBusiness.class);
+	}
+	
+	public SchoolTypeHome getSchoolTypeHome() throws RemoteException {
+		return (SchoolTypeHome) IDOLookup.getHome(SchoolType.class);	
 	}
 
 	public UserHome getUserHome() throws RemoteException {
