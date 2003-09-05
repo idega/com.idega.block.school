5fd1a090c58cf78a923bb3524318c9721106a8f9
Grimur Jonsson <gimmi@idega.com>
2003-09-05 15:27:29 +0000

    getSchoolYearAge no longer throws RemoteException

--- src/java/com/idega/block/school/business/SchoolYearComparator.java
+++ src/java/com/idega/block/school/business/SchoolYearComparator.java
@@ -1,6 +1,5 @@
 package com.idega.block.school.business;
 
-import java.rmi.RemoteException;
 import java.util.Comparator;
 
 import com.idega.block.school.data.SchoolYear;
@@ -18,17 +17,8 @@ public class SchoolYearComparator implements Comparator {
 		SchoolYear schoolYear1 = (SchoolYear) o1;
 		SchoolYear schoolYear2 = (SchoolYear) o2;
 		
-		int yearAge1 = -1;
-		int yearAge2 = -1;
-		
-		try {
-			yearAge1 = schoolYear1.getSchoolYearAge();
-			yearAge2 = schoolYear2.getSchoolYearAge();
-		}
-		catch (RemoteException re) {
-			yearAge1 = -1;
-			yearAge2 = -1;
-		}
+		int yearAge1 = schoolYear1.getSchoolYearAge();
+		int yearAge2 = schoolYear2.getSchoolYearAge();
 		
 		if (yearAge1 < yearAge2)
 			return -1;
