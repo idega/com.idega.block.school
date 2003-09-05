4c50b2e693c938559905a0c4975d107aab56e885
Grimur Jonsson <gimmi@idega.com>
2003-09-05 15:25:13 +0000

    *** empty log message ***

---
+++ src/java/com/idega/block/school/presentation/SchoolCourseEditor.java
@@ -0,0 +1,263 @@
+package com.idega.block.school.presentation;
+
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.idega.block.school.business.SchoolBusiness;
+import com.idega.block.school.data.School;
+import com.idega.block.school.data.SchoolCourse;
+import com.idega.block.school.data.SchoolCourseHome;
+import com.idega.business.IBOLookup;
+import com.idega.data.IDOLookup;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.Block;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Link;
+import com.idega.presentation.ui.BackButton;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.presentation.ui.Form;
+import com.idega.presentation.ui.SubmitButton;
+import com.idega.presentation.ui.TextArea;
+import com.idega.presentation.ui.TextInput;
+import com.idega.util.text.TextFormat;
+
+/**
+ * @author Gimmi
+ */
+public class SchoolCourseEditor extends Block {
+	private TextFormat tFormat;
+	private SchoolBusiness sBus;
+	private IWResourceBundle iwrb;
+
+	private String ACTION = "sce_a";
+	private String ACTION_PARAMETER_NEW_COURSE = "sce_nc";
+	private String ACTION_PARAMETER_SAVE_COURSE = "sce_sc";
+	private String ACTION_PARAMETER_DELETE_COURSE = "sce_ec";
+	private String ACTION_PARAMETER_EDIT_COURSE = "sce_dc";
+
+	private String PARAMETER_SCHOOL_ID = "sce_s_id";
+	private String PARAMETER_SCHOOL_TYPE_ID = "sce_st_id";
+	private String PARAMETER_SCHOOL_COURSE_ID = "sce_sc_id";
+	private String PARAMETER_COURSE_NAME = "sce_cn";
+	private String PARAMETER_COURSE_DESCRIPTION = "sce_cd";
+	private School school;
+	private String schoolTypeId;
+
+	public final static String IW_BUNDLE_IDENTIFIER = "com.idega.block.school";
+
+	public String getBundleIdentifier(){
+	  return IW_BUNDLE_IDENTIFIER;
+	}
+	
+	private void init(IWContext iwc) throws RemoteException {
+		iwrb = getResourceBundle(iwc);
+		tFormat = tFormat.getInstance();
+		sBus = (SchoolBusiness) IBOLookup.getServiceInstance(iwc, SchoolBusiness.class);
+		String sSchoolId = iwc.getParameter(PARAMETER_SCHOOL_ID);
+		if (sSchoolId != null) {
+			school = sBus.getSchool(new Integer(sSchoolId));
+		}
+		schoolTypeId = iwc.getParameter(PARAMETER_SCHOOL_TYPE_ID);
+	}
+	
+	public void main (IWContext iwc) throws RemoteException {
+		init(iwc);
+		
+		String action = iwc.getParameter(ACTION);
+		if (action == null) {
+			action = "";
+		}
+		if ( action.equals(ACTION_PARAMETER_NEW_COURSE) || action.equals(ACTION_PARAMETER_EDIT_COURSE) ) {
+			createCourse(iwc);
+		} else if ( action.equals(ACTION_PARAMETER_SAVE_COURSE)) {
+			if (!saveCourse(iwc)) {
+				add(tFormat.format(iwrb.getLocalizedString("school.could_not_save_course","Could not save course"), TextFormat.HEADER));
+			} 
+			drawMenu(iwc);
+		} else if (action.equals(ACTION_PARAMETER_DELETE_COURSE)) {
+			if (deleteCourse(iwc)) {
+				add(tFormat.format(iwrb.getLocalizedString("school.could_not_delete_course","Could not delete course"), TextFormat.HEADER));
+			} 
+			drawMenu(iwc);
+		} else {
+			drawMenu(iwc);
+		}
+	}
+	
+	private void drawMenu(IWContext iwc) throws RemoteException {
+		Form form = new Form();
+		Table table = new Table();
+		int row = 1;
+
+		form.maintainParameter(PARAMETER_SCHOOL_ID);
+		form.maintainParameter(PARAMETER_SCHOOL_TYPE_ID);
+
+		Collection schoolColl = sBus.findAllSchools();
+		if (schoolColl != null && !schoolColl.isEmpty()) {
+			DropdownMenu schools = new DropdownMenu(schoolColl, PARAMETER_SCHOOL_ID);
+			DropdownMenu schoolTypes = new DropdownMenu(PARAMETER_SCHOOL_TYPE_ID);
+			schools.setToSubmit();
+			schoolTypes.setToSubmit();
+			if (school != null) {
+				schools.setSelectedElement(school.getPrimaryKey().toString());
+				Map schoolTypesMap = sBus.getSchoolRelatedSchoolTypes(school);
+				if (schoolTypesMap != null) {
+					schoolTypes.addMenuElements(schoolTypesMap.values());
+				}
+				if (schoolTypeId != null) {
+					schoolTypes.setSelectedElement(schoolTypeId);
+				}
+			}
+				
+			table.add(schools, 1, row);
+			table.add(schoolTypes, 1, row);
+			table.add(new SubmitButton(iwrb.getLocalizedImageButton("school.submit","Submit")), 1, row);
+			table.mergeCells(1, row, 2, row);
+			
+			if (schoolTypeId != null) {
+				row = listCourses(iwc, table, row);
+				SubmitButton newCourse = new SubmitButton(iwrb.getLocalizedImageButton("school.new","New"), ACTION, ACTION_PARAMETER_NEW_COURSE);
+				table.add(newCourse, 1, ++row);
+			}
+			
+		}
+		
+		
+		form.add(table);
+		add(form);
+	}
+	
+	private int listCourses(IWContext iwc, Table table, int row) throws RemoteException {
+		Map map = sBus.getSchoolAndSchoolTypeRelatedSchoolCourses(school, schoolTypeId);
+
+		++row;
+		table.add(tFormat.format(iwrb.getLocalizedString("school.name","Name"), TextFormat.HEADER), 1, row);
+		table.add(tFormat.format(iwrb.getLocalizedString("school.description","Description"), TextFormat.HEADER), 2, row);
+		
+		if (map != null && !map.isEmpty()) {
+			Collection courses = map.values();
+			Iterator iter = courses.iterator();
+			SchoolCourse course;
+			Link edit;
+			Link delete;
+			while (iter.hasNext()) {
+				course = (SchoolCourse) iter.next();
+				++row;
+				table.add(course.getName(), 1, row);
+				table.add(course.getDescription(), 2, row);
+				
+				edit = new Link(iwrb.getLocalizedImageButton("school.edit","Edit"));
+				edit.maintainParameter(PARAMETER_SCHOOL_ID, iwc);
+				edit.maintainParameter(PARAMETER_SCHOOL_TYPE_ID, iwc);
+				edit.addParameter(ACTION, ACTION_PARAMETER_EDIT_COURSE);
+				edit.addParameter(PARAMETER_SCHOOL_COURSE_ID, course.getPrimaryKey().toString());
+				
+				delete = new Link(iwrb.getLocalizedImageButton("school.delete","Delete"));
+				delete.maintainParameter(PARAMETER_SCHOOL_ID, iwc);
+				delete.maintainParameter(PARAMETER_SCHOOL_TYPE_ID, iwc);
+				delete.addParameter(ACTION, ACTION_PARAMETER_DELETE_COURSE);
+				delete.addParameter(PARAMETER_SCHOOL_COURSE_ID, course.getPrimaryKey().toString());
+
+				table.add(edit, 3, row);
+				table.add(delete, 3, row);
+			}
+		} else {
+			++row;
+			table.add(tFormat.format(iwrb.getLocalizedString("school.no_courses_found","No courses found"), TextFormat.NORMAL), 1, row);
+		}
+		return row;
+	}
+	
+	private void createCourse(IWContext iwc) {
+		Form form = new Form();
+		Table table = new Table();
+
+		form.maintainParameter(PARAMETER_SCHOOL_ID);
+		form.maintainParameter(PARAMETER_SCHOOL_TYPE_ID);
+		
+		TextInput name = new TextInput(PARAMETER_COURSE_NAME);
+		TextArea description = new TextArea(PARAMETER_COURSE_DESCRIPTION);
+		
+		String schoolCourseId = iwc.getParameter(PARAMETER_SCHOOL_COURSE_ID);
+		if (schoolCourseId != null) {
+			try {
+				SchoolCourseHome scHome = (SchoolCourseHome) IDOLookup.getHome(SchoolCourse.class);
+				SchoolCourse course = scHome.findByPrimaryKey(new Integer(schoolCourseId));
+				form.maintainParameter(PARAMETER_SCHOOL_COURSE_ID);
+				name.setContent(course.getName());
+				description.setContent(course.getDescription());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		table.mergeCells(1, 1, 2, 1);
+		table.add(tFormat.format(iwrb.getLocalizedString("school.course","Course"), TextFormat.HEADER), 1, 1);
+		
+		table.add(tFormat.format(iwrb.getLocalizedString("school.name","Name"), TextFormat.NORMAL), 1, 2);
+		table.add(name, 2, 2);
+		
+		table.add(tFormat.format(iwrb.getLocalizedString("school.description","Description"), TextFormat.NORMAL), 1, 3);
+		table.add(description, 2, 3);
+		
+		SubmitButton save = new SubmitButton(iwrb.getLocalizedImageButton("school.save","Save"), ACTION, ACTION_PARAMETER_SAVE_COURSE);
+		BackButton back = new BackButton(iwrb.getLocalizedImageButton("school.Cancel", "Cancel"));
+		
+		table.add(save, 2, 4);
+		table.add(back, 2, 4);
+		
+		form.add(table);
+		add(form);
+	}
+	
+	private boolean saveCourse(IWContext iwc) {
+		String name = iwc.getParameter(PARAMETER_COURSE_NAME);
+		String description = iwc.getParameter(PARAMETER_COURSE_DESCRIPTION);
+		
+		String schoolCourseId = iwc.getParameter(PARAMETER_SCHOOL_COURSE_ID);
+		if (name != null && !name.equals("") ) {
+			try {
+				SchoolCourseHome scHome = (SchoolCourseHome) IDOLookup.getHome(SchoolCourse.class);
+				SchoolCourse course;
+				if (schoolCourseId == null) {
+					course = scHome.create();
+				} else {
+					course = scHome.findByPrimaryKey(new Integer(schoolCourseId));
+				}
+				course.setName(name);
+				course.setDescription(description);
+				course.setSchoolPk(school.getPrimaryKey());
+				course.setSchoolTypePk(new Integer(schoolTypeId));
+				course.store();
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
+	private boolean deleteCourse(IWContext iwc) {
+		String schoolCourseId = iwc.getParameter(PARAMETER_SCHOOL_COURSE_ID);
+		
+		if (schoolCourseId != null) {
+			try {
+				SchoolCourseHome scHome = (SchoolCourseHome) IDOLookup.getHome(SchoolCourse.class);
+				SchoolCourse course = scHome.findByPrimaryKey(new Integer(schoolCourseId));
+				course.removeAllSchoolClassMembers();
+				course.remove();
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return false;
+	}
+
+}
