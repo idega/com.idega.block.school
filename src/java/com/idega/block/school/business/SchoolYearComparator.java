e779be97f3026cd585c30c3befa37277fd744da6
Thorhallur Helgason <laddi@idega.com>
2003-11-05 14:37:09 +0000

    Changed so that the school years are sorted by type and then by age.

--- src/java/com/idega/block/school/business/SchoolYearComparator.java
+++ src/java/com/idega/block/school/business/SchoolYearComparator.java
@@ -14,15 +14,29 @@ public class SchoolYearComparator implements Comparator {
 	 * @see java.util.Comparator#compare(Object, Object)
 	 */
 	public int compare(Object o1, Object o2) {
+		int returner = 0;
 		SchoolYear schoolYear1 = (SchoolYear) o1;
 		SchoolYear schoolYear2 = (SchoolYear) o2;
 		
-		int yearAge1 = schoolYear1.getSchoolYearAge();
-		int yearAge2 = schoolYear2.getSchoolYearAge();
-		
-		if (yearAge1 < yearAge2)
+		returner = compareTypes(schoolYear1.getSchoolTypeId(), schoolYear2.getSchoolTypeId());
+		if (returner == 0)
+			returner = compareAge(schoolYear1.getSchoolYearAge(), schoolYear2.getSchoolYearAge());
+		return returner;
+	}
+	
+	public int compareTypes(int schoolType1, int schoolType2) {
+		if (schoolType1 < schoolType2)
 			return -1;
-		else if (yearAge1 > yearAge2)
+		else if (schoolType1 > schoolType2)
+			return 1;
+		else
+			return 0;
+	}
+
+	public int compareAge(int age1, int age2) {
+		if (age1 < age2)
+			return -1;
+		else if (age1 > age2)
 			return 1;
 		else
 			return 0;
