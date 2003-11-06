fb77308549fbb217f8c3db5b94807d1a67d7499e
Thorhallur Helgason <laddi@idega.com>
2003-11-06 18:50:24 +0000

    Changed so that name is quoted in SQL queries.

--- src/java/com/idega/block/school/data/SchoolManagementTypeBMPBean.java
+++ src/java/com/idega/block/school/data/SchoolManagementTypeBMPBean.java
@@ -114,26 +114,26 @@ public class SchoolManagementTypeBMPBean extends GenericEntity implements School
 
 	public Integer ejbFindManagementType(String name) throws FinderException {
 		IDOQuery query = idoQuery();
-		query.appendSelectAllFrom(this).appendWhereEquals(COLUMN_NAME, name);
+		query.appendSelectAllFrom(this).appendWhereEqualsQuoted(COLUMN_NAME, name);
 		
 		return (Integer) idoFindOnePKByQuery(query);
 	}
 	
 	public Integer ejbFindPrivateManagementType() throws FinderException {
 		IDOQuery query = idoQuery();
-		query.appendSelectAllFrom(this).appendWhereEquals(COLUMN_NAME, TYPE_PRIVATE);
+		query.appendSelectAllFrom(this).appendWhereEqualsQuoted(COLUMN_NAME, TYPE_PRIVATE);
 		return (Integer) idoFindOnePKByQuery(query);
 	}
 	
 	public Integer ejbFindPublicManagementType() throws FinderException {
 		IDOQuery query = idoQuery();
-		query.appendSelectAllFrom(this).appendWhereEquals(COLUMN_NAME, TYPE_PUBLIC);
+		query.appendSelectAllFrom(this).appendWhereEqualsQuoted(COLUMN_NAME, TYPE_PUBLIC);
 		return (Integer) idoFindOnePKByQuery(query);
 	}
 	
 	public Integer ejbFindCooperativeManagementType() throws FinderException {
 		IDOQuery query = idoQuery();
-		query.appendSelectAllFrom(this).appendWhereEquals(COLUMN_NAME, TYPE_COOPERATIVE);
+		query.appendSelectAllFrom(this).appendWhereEqualsQuoted(COLUMN_NAME, TYPE_COOPERATIVE);
 		return (Integer) idoFindOnePKByQuery(query);
 	}
 	
