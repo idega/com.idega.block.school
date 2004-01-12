00e0a8cc8781b3a47cf81e072516afa5bdfc4ea2
Anders Lindman <anders.lindman@ncmedia.com>
2004-01-12 12:18:20 +0000

    Added findAllByAreaTypeManagementCommune()

--- src/java/com/idega/block/school/data/SchoolBMPBean.java
+++ src/java/com/idega/block/school/data/SchoolBMPBean.java
@@ -358,6 +358,27 @@ public class SchoolBMPBean extends GenericEntity  implements School, IDOLegacyEn
 		return super.idoFindPKsBySQL(select);
 	}
 
+	public Collection ejbFindAllByAreaTypeManagementCommune(int areaId, int typeId, Collection managementTypes, int communeId) throws javax.ejb.FinderException {
+		String select = "select s.* from " + SCHOOL + " s,sch_school_sch_school_type m where m.sch_school_type_id = " + typeId + " and m.sch_school_id = s.sch_school_id " +
+				" and " + SCHOOLAREA + " = " + areaId + 
+				" and " + COMMUNE + " = " + communeId + 
+				" and (termination_date is null or termination_date > '" + getCurrentDate() + "')";
+		if (managementTypes != null && !managementTypes.isEmpty()) {
+			select += (" and s." + MANAGEMENT_TYPE + " in (");
+			Iterator it = managementTypes.iterator();
+			String type = null;
+			while (it.hasNext()) {
+				type = (String) it.next();
+				select += "'" + type + "'";
+				if (it.hasNext())
+					select += ",";					
+			}
+			select += ")";
+		}
+		select += " order by s."+NAME;
+		return super.idoFindPKsBySQL(select);
+	}
+
 	public Collection ejbFindAllByAreaTypeCommune(int areaId, int typeId, int communeId) throws javax.ejb.FinderException {
 		String select = "select s.* from " + SCHOOL + " s,sch_school_sch_school_type m where m.sch_school_type_id = " + typeId + " and m.sch_school_id = s.sch_school_id " +
 				" and " + SCHOOLAREA + " = " + areaId + 
