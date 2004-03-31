19137c7ae4c9d7a6f7f2f07e2495d8a68c72845f
Thorhallur Helgason <laddi@idega.com>
2004-03-31 09:35:06 +0000

    A new Comparator to sort school classes, sorts by main/sub groups and then by name.

---
+++ src/java/com/idega/block/school/business/SchoolClassComparator.java
@@ -0,0 +1,39 @@
+package com.idega.block.school.business;
+
+import java.text.Collator;
+import java.util.Comparator;
+
+import com.idega.block.school.data.SchoolClass;
+
+/**
+ * A class to compare a collection of <code>SchoolClass</code> objects.
+ * @author Laddi
+ */
+public class SchoolClassComparator implements Comparator {
+	
+	/**
+	 * @see java.util.Comparator#compare(Object, Object)
+	 */
+	public int compare(Object o1, Object o2) {
+		int returner = 0;
+		SchoolClass schoolClass1 = (SchoolClass) o1;
+		SchoolClass schoolClass2 = (SchoolClass) o2;
+		
+		returner = compareSubGroups(schoolClass1.getIsSubGroup(), schoolClass2.getIsSubGroup());
+		
+		if (returner == 0)
+			returner = Collator.getInstance().compare(schoolClass1.getSchoolClassName(), schoolClass2.getSchoolClassName());
+
+		return returner;
+	}
+	
+	public int compareSubGroups(boolean subGroup1, boolean subGroup2) {
+		if (!subGroup1 && subGroup2)
+			return -1;
+		else if (subGroup1 && !subGroup2)
+			return 1;
+		else
+			return 0;
+	}
+
+}
