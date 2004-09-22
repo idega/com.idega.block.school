cfc518d4b0f49d9b2afad1824fa8cbe790a61722
Aron Birkir Gudmundsson <aron@idega.com>
2004-09-22 11:01:43 +0000

    findBySchool sql update to SelectQuery

--- src/java/com/idega/block/school/data/SchoolClassMemberBMPBean.java
+++ src/java/com/idega/block/school/data/SchoolClassMemberBMPBean.java
@@ -51,8 +51,8 @@ import com.idega.util.IWTimestamp;
  * 
  * @author <br>
  *         <a href="mailto:aron@idega.is">Aron Birkir </a> <br>
- *         Last modified: $Date: 2004/09/15 09:34:09 $ by $Author: gummi $
- * @version $Revision: 1.121 $
+ *         Last modified: $Date: 2004/09/22 11:01:43 $ by $Author: aron $
+ * @version $Revision: 1.122 $
  */
 
 public class SchoolClassMemberBMPBean extends GenericEntity implements SchoolClassMember {
@@ -943,19 +943,33 @@ public class SchoolClassMemberBMPBean extends GenericEntity implements SchoolCla
 	}
 
 	public Collection ejbFindBySchool(int schoolID, int schoolClassID, String schoolCategory, Date date) throws FinderException {
-		IDOQuery sql = idoQuery();
-		sql.appendSelectAllFrom(this.getTableName() + " mb" + "," + SchoolClassBMPBean.SCHOOLCLASS + " cl, sch_school_type t, ic_user u").appendWhere().append(" cl." + SchoolClassBMPBean.SCHOOL).appendEqualSign().append(schoolID).appendAndEquals("u.ic_user_id", "mb." + MEMBER).appendAnd().append("(cl." + SchoolClassBMPBean.COLUMN_VALID).appendEqualSign().appendWithinSingleQuotes("Y").appendOr().append("cl." + SchoolClassBMPBean.COLUMN_VALID).append(" is null)").appendAnd().append(" mb." + SCHOOLCLASS).appendEqualSign().append("cl." + SchoolClassBMPBean.SCHOOLCLASS + "_id");
-		sql.appendAndEquals("mb." + SCHOOL_TYPE, "t.sch_school_type_id");
-		if (schoolCategory != null)
-			sql.appendAndEqualsQuoted("t.school_category", schoolCategory);
-		if (schoolClassID != -1)
-			sql.appendAndEquals("mb." + SCHOOLCLASS, schoolClassID);
-		//sql.appendAnd().appendLeftParenthesis().append(REGISTER_DATE).appendLessThanOrEqualsSign().append(date)
-		//.appendOr().append(REGISTER_DATE).append(" is
-		// null").appendRightParenthesis();
-		sql.appendAnd().appendLeftParenthesis().append(REMOVED_DATE).appendGreaterThanOrEqualsSign().append(date).appendOr().append(REMOVED_DATE).append(" is null").appendRightParenthesis();
-		sql.appendOrderBy("u.last_name, u.first_name, u.middle_name");
-		return super.idoFindPKsBySQL(sql.toString());
+		
+            Table tMember = new Table(this,"A");
+            Table tClass = new Table(SchoolClass.class,"B");
+            Table tType = new Table(SchoolType.class,"C");
+            Table tUser = new Table(com.idega.core.user.data.User.class,"D");
+            SelectQuery query = new SelectQuery(tMember);
+         try {
+             
+            query.addJoin(tMember,tType);
+            query.addJoin(tMember,tUser);
+            query.addJoin(tMember,tClass);
+            query.addColumn(new WildCardColumn(tMember));
+            query.addCriteria(new MatchCriteria(tClass,SchoolClassBMPBean.SCHOOL,MatchCriteria.EQUALS,schoolID));
+            query.addCriteria(new OR(new MatchCriteria(tClass,SchoolClassBMPBean.COLUMN_VALID,MatchCriteria.EQUALS,true),new MatchCriteria(tClass,SchoolClassBMPBean.COLUMN_VALID,MatchCriteria.IS,MatchCriteria.NULL)));
+            if(schoolCategory!=null)
+                query.addCriteria(new MatchCriteria(tType,SchoolTypeBMPBean.SCHOOLCATEGORY,MatchCriteria.EQUALS,schoolCategory));
+            if(schoolClassID!=-1)
+                query.addCriteria(new MatchCriteria(tMember,SCHOOLCLASS,MatchCriteria.EQUALS,schoolClassID));
+            query.addCriteria(new OR(new MatchCriteria(tMember,REMOVED_DATE,MatchCriteria.GREATEREQUAL,date),new MatchCriteria(tMember,REMOVED_DATE,MatchCriteria.IS,MatchCriteria.NULL)));
+            query.addOrder(tUser,"last_name",true);
+            query.addOrder(tUser,"first_name",true);
+            query.addOrder(tUser,"middle_name",true);
+            return idoFindPKsByQuery(query);
+        } catch (IDORelationshipException e) {
+            throw new FinderException(e.getMessage());
+        }
+        
 	}
 
 	public Collection ejbFindBySchool(int schoolID, int schoolClassID, Date date, boolean showNotYetActive) throws FinderException {
