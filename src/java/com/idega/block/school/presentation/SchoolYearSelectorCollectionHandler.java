defddc724104b5f36305699e09297f7000164839
Thorhallur Helgason <laddi@idega.com>
2005-01-07 10:05:30 +0000

    Added localized support.

--- src/java/com/idega/block/school/presentation/SchoolYearSelectorCollectionHandler.java
+++ src/java/com/idega/block/school/presentation/SchoolYearSelectorCollectionHandler.java
@@ -16,6 +16,7 @@ import java.util.Vector;
 import com.idega.block.school.data.SchoolYear;
 import com.idega.block.school.data.SchoolYearHome;
 import com.idega.data.IDOLookup;
+import com.idega.idegaweb.IWResourceBundle;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.remotescripting.RemoteScriptCollection;
 import com.idega.presentation.remotescripting.RemoteScriptHandler;
@@ -23,10 +24,10 @@ import com.idega.presentation.remotescripting.RemoteScriptingResults;
 
 /**
  * 
- * Last modified: $Date: 2005/01/04 13:49:52 $ by $Author: laddi $
+ * Last modified: $Date: 2005/01/07 10:05:30 $ by $Author: laddi $
  * 
  * @author <a href="mailto:aron@idega.com">aron </a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class SchoolYearSelectorCollectionHandler implements RemoteScriptCollection {
 
@@ -36,14 +37,15 @@ public class SchoolYearSelectorCollectionHandler implements RemoteScriptCollecti
 	 * @see com.idega.presentation.remotescripting.RemoteScriptCollection#getResults(com.idega.presentation.IWContext)
 	 */
 	public RemoteScriptingResults getResults(IWContext iwc) {
+		IWResourceBundle iwrb = iwc.getIWMainApplication().getBundle("com.idega.block.school").getResourceBundle(iwc.getCurrentLocale());
 		String sourceName = iwc.getParameter(RemoteScriptHandler.PARAMETER_SOURCE_PARAMETER_NAME);
 
 		String sourceID = iwc.getParameter(sourceName);
 
-		return handleYearUpdate(sourceName, sourceID);
+		return handleYearUpdate(sourceName, sourceID, iwrb);
 	}
 
-	private RemoteScriptingResults handleYearUpdate(String sourceName, String typeID) {
+	private RemoteScriptingResults handleYearUpdate(String sourceName, String typeID, IWResourceBundle iwrb) {
 		if (typeID != null) {
 
 			try {
@@ -56,7 +58,7 @@ public class SchoolYearSelectorCollectionHandler implements RemoteScriptCollecti
 				Iterator iter = schools.iterator();
 				if (iter.hasNext()) {
 					ids.add("-1");
-					names.add("Select");
+					names.add(iwrb.getLocalizedString("select_year", "Select year"));
 				}
 				while (iter.hasNext()) {
 					SchoolYear s = (SchoolYear) iter.next();
@@ -66,7 +68,7 @@ public class SchoolYearSelectorCollectionHandler implements RemoteScriptCollecti
 
 				if (schools.isEmpty()) {
 					ids.add("-1");
-					names.add("Unavailable");
+					names.add(iwrb.getLocalizedString("unavailable", "Unavailable"));
 				}
 
 				RemoteScriptingResults rsr = new RemoteScriptingResults(RemoteScriptHandler.getLayerName(sourceName, "id"), ids);
