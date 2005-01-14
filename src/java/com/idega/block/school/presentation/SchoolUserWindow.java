0a2a7690eeeb0678e6bdc9cf0590a1dd4581d56d
Anders Lindman <anders.lindman@ncmedia.com>
2005-01-14 08:25:40 +0000

    Initial commit

---
+++ src/java/com/idega/block/school/presentation/SchoolUserWindow.java
@@ -0,0 +1,30 @@
+package com.idega.block.school.presentation;
+
+import com.idega.presentation.IWContext;
+import com.idega.presentation.ui.Window;
+
+/**
+ * @author anders
+ */
+public class SchoolUserWindow extends Window {
+
+	public SchoolUserWindow() {
+		this.setWidth(600);
+		this.setHeight(600);
+		this.setScrollbar(true);
+		this.setResizable(true);	
+		this.setAllMargins(0);
+	}
+
+	/**
+	 * @see com.idega.presentation.PresentationObject#main(IWContext)
+	 */
+	public void main(IWContext iwc) throws Exception {
+//		if (iwc.isParameterSet("sge_action")) {
+			this.setParentToReload();
+//		}
+		SchoolUserEditor e = new SchoolUserEditor();
+		e.setHideLogin(true);
+		add(e);
+	}	
+}
