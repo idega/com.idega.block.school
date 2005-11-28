40942eaa34bb2a9568724be9c17f812c3ac662dc
Dainis Brjuhoveckis <dainis.brjuhoveckis@idega.lv>
2005-11-28 18:48:26 +0000

    merged in handicraft related changes from HEAD

--- src/java/com/idega/block/school/data/SchoolStudyPathGroup.java
+++ src/java/com/idega/block/school/data/SchoolStudyPathGroup.java
@@ -1,11 +1,5 @@
-/*
- * $Id: SchoolStudyPathGroup.java,v 1.2 2005/05/11 07:14:19 laddi Exp $
- * Created on 28.4.2005
- *
- * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
- *
- * This software is the proprietary information of Idega hf.
- * Use is subject to license terms.
+/**
+ * 
  */
 package com.idega.block.school.data;
 
@@ -13,10 +7,13 @@ import com.idega.data.IDOEntity;
 
 
 /**
- * Last modified: $Date: 2005/05/11 07:14:19 $ by $Author: laddi $
+ * <p>
+ * TODO Dainis Describe Type SchoolStudyPathGroup
+ * </p>
+ *  Last modified: $Date: 2005/11/28 18:48:26 $ by $Author: dainis $
  * 
- * @author <a href="mailto:laddi@idega.com">laddi</a>
- * @version $Revision: 1.2 $
+ * @author <a href="mailto:Dainis@idega.com">Dainis</a>
+ * @version $Revision: 1.2.2.1 $
  */
 public interface SchoolStudyPathGroup extends IDOEntity {
 
@@ -39,4 +36,14 @@ public interface SchoolStudyPathGroup extends IDOEntity {
 	 * @see com.idega.block.school.data.SchoolStudyPathGroupBMPBean#setLocalizationKey
 	 */
 	public void setLocalizationKey(String localizedkey);
+
+	/**
+	 * @see com.idega.block.school.data.SchoolStudyPathGroupBMPBean#getSchoolType
+	 */
+	public SchoolType getSchoolType();
+
+	/**
+	 * @see com.idega.block.school.data.SchoolStudyPathGroupBMPBean#setSchoolType
+	 */
+	public void setSchoolType(SchoolType type);
 }
--- src/java/com/idega/block/school/data/SchoolStudyPathGroupHome.java
+++ src/java/com/idega/block/school/data/SchoolStudyPathGroupHome.java
@@ -1,11 +1,5 @@
-/*
- * $Id: SchoolStudyPathGroupHome.java,v 1.2 2005/05/11 07:14:19 laddi Exp $
- * Created on 28.4.2005
- *
- * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
- *
- * This software is the proprietary information of Idega hf.
- * Use is subject to license terms.
+/**
+ * 
  */
 package com.idega.block.school.data;
 
@@ -15,10 +9,13 @@ import com.idega.data.IDOHome;
 
 
 /**
- * Last modified: $Date: 2005/05/11 07:14:19 $ by $Author: laddi $
+ * <p>
+ * TODO Dainis Describe Type SchoolStudyPathGroupHome
+ * </p>
+ *  Last modified: $Date: 2005/11/28 18:48:26 $ by $Author: dainis $
  * 
- * @author <a href="mailto:laddi@idega.com">laddi</a>
- * @version $Revision: 1.2 $
+ * @author <a href="mailto:Dainis@idega.com">Dainis</a>
+ * @version $Revision: 1.2.2.1 $
  */
 public interface SchoolStudyPathGroupHome extends IDOHome {
 
