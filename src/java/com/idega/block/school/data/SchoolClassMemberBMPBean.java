fd020f274ac35ea8af03978f2f2b881728838d8d
Dainis Brjuhoveckis <dainis.brjuhoveckis@idega.lv>
2006-01-16 09:47:07 +0000

    removed unused import

--- src/java/com/idega/block/school/data/SchoolClassMemberBMPBean.java
+++ src/java/com/idega/block/school/data/SchoolClassMemberBMPBean.java
@@ -39,7 +39,6 @@ import com.idega.user.data.GroupRelation;
 import com.idega.user.data.User;
 import com.idega.user.data.UserBMPBean;
 import com.idega.util.IWTimestamp;
-import com.sun.org.apache.xerces.internal.impl.dv.dtd.NOTATIONDatatypeValidator;
 
 /**
  * <p>
@@ -57,8 +56,8 @@ import com.sun.org.apache.xerces.internal.impl.dv.dtd.NOTATIONDatatypeValidator;
  * 
  * @author <br>
  *         <a href="mailto:aron@idega.is">Aron Birkir </a> <br>
- *         Last modified: $Date: 2006/01/16 09:38:59 $ by $Author: dainis $
- * @version $Revision: 1.160 $
+ *         Last modified: $Date: 2006/01/16 09:47:07 $ by $Author: dainis $
+ * @version $Revision: 1.161 $
  */
 
 public class SchoolClassMemberBMPBean extends GenericEntity implements SchoolClassMember {
