378a71ab0a6c17b7dbe9b89f1bc9e0eabe71153d
Thorhallur Helgason <laddi@idega.com>
2006-02-13 09:53:08 +0000

    Added back in missing fields.

--- src/java/com/idega/block/school/data/SchoolClassMember.java
+++ src/java/com/idega/block/school/data/SchoolClassMember.java
@@ -1,5 +1,5 @@
 /*
- * $Id: SchoolClassMember.java,v 1.54 2006/02/10 09:20:17 laddi Exp $
+ * $Id: SchoolClassMember.java,v 1.55 2006/02/13 09:53:08 laddi Exp $
  * Created on Feb 10, 2006
  *
  * Copyright (C) 2006 Idega Software hf. All Rights Reserved.
@@ -22,12 +22,28 @@ import com.idega.user.data.User;
  * <p>
  * TODO laddi Describe Type SchoolClassMember
  * </p>
- *  Last modified: $Date: 2006/02/10 09:20:17 $ by $Author: laddi $
+ *  Last modified: $Date: 2006/02/13 09:53:08 $ by $Author: laddi $
  * 
  * @author <a href="mailto:laddi@idega.com">laddi</a>
- * @version $Revision: 1.54 $
+ * @version $Revision: 1.55 $
  */
 public interface SchoolClassMember extends IDOEntity {
+
+	public final static String FIELD_SCHOOLCLASSMEMBERID = "sch_class_member_id";
+	public final static String FIELD_SCHOOLCLASSMEMBER = "sch_class_member";
+	public final static String FIELD_MEMBER = "ic_user_id";
+	public final static String FIELD_NOTES = "notes";
+	public final static String FIELD_SCHOOLCLASS = "sch_school_class_id";
+	public final static String FIELD_REGISTER_DATE = "register_date";
+	public final static String FIELD_REMOVED_DATE = "removed_date";
+	public final static String FIELD_REGISTRATOR = "registrator";
+	public final static String FIELD_NEEDS_SPECIAL_ATTENTION = "NEEDS_SPECIAL_ATTENTION";
+	public final static String FIELD_SPECIALLY_PLACED = "SPECIALLY_PLACED";
+	public final static String FIELD_LANGUAGE = "LANGUAGE";
+	//Added for the kompliterings project
+	public final static String FIELD_COMPENSATION_BY_INVOICE = "comp_by_invoice";
+	public final static String FIELD_INVOICE_INTERVAL = "invoice_int";
+	public final static String FIELD_LATEST_INVOICE_DATE = "latest_invoice_date";		
 
 	/**
 	 * @see com.idega.block.school.data.SchoolClassMemberBMPBean#setClassMemberId
