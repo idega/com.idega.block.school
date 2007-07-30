e76765a13dc077962da8f2fdd0cdf789594ed4ad
Aleksandras Skrynikovas <alexis@idega.com>
2007-07-30 15:46:56 +0000

    q

--- src/java/com/idega/block/school/presentation/SchoolEditor.java
+++ src/java/com/idega/block/school/presentation/SchoolEditor.java
@@ -7,7 +7,6 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 import javax.ejb.FinderException;
 
@@ -50,8 +49,6 @@ import com.idega.presentation.ui.util.SelectorUtility;
  */
 
 public class SchoolEditor extends SchoolBlock {
-	
-	private static final Logger logger = Logger.getLogger(SchoolEditor.class.toString());
 
 	private static final String PARAMETER_ACTION = "sch_prm_action";
 	private static final String PARAMETER_SCHOOL_PK = "prm_school_pk";
@@ -210,7 +207,6 @@ public class SchoolEditor extends SchoolBlock {
 		boolean hasPostCare = iwc.isParameterSet(PARAMETER_HAS_POST_CARE) ? new Boolean(iwc.getParameter(PARAMETER_HAS_POST_CARE)).booleanValue() : false;
 		boolean hasHandicap = iwc.isParameterSet(PARAMETER_HAS_HANDICAP) ? new Boolean(iwc.isParameterSet(PARAMETER_HAS_POST_CARE)).booleanValue() : false;
 		
-		logger.info("Saving School Entity with property hasHandicap: " + hasHandicap);
 		
 		School school = getBusiness().storeSchool(sid, name, info, address, zipcode, ziparea, phone, null, null, null, areaId, types, years, communePK, providerStringId);
 		if (juniorHighID != null) {
@@ -275,16 +271,13 @@ public class SchoolEditor extends SchoolBlock {
 
 		Collection schools = new java.util.Vector(0);
 		if (this.iSchoolCategory == null) {
-			logger.info("Finding all schools");
 			schools = getBusiness().findAllSchools();
 		}
 		else {
 			if (getSchoolTypePK(iwc) != null) {
-				logger.info("Finding all schools by type: " + Integer.parseInt(getSchoolTypePK(iwc).toString()));
 				schools = getBusiness().findAllSchoolsByType(Integer.parseInt(getSchoolTypePK(iwc).toString()));
 			}
 			else {
-				logger.info("Finding all schools by category: " + this.iSchoolCategory);
 				schools = getBusiness().findAllSchoolsByCategory(this.iSchoolCategory);
 			}
 		}
@@ -483,7 +476,6 @@ public class SchoolEditor extends SchoolBlock {
 				hasReview.setSelectedElement(String.valueOf(school.hasReview()));
 				hasPreCare.setSelectedElement(String.valueOf(school.hasPreCare()));
 				hasPostCare.setSelectedElement(String.valueOf(school.hasPostCare()));
-				logger.info("Rendering SchoolEditor Has Handicap facilities: " + school.hasHandicap());
 				hasHandicap.setChecked(school.hasHandicap());
 			}
 			catch (Exception ex) {
@@ -705,12 +697,12 @@ public class SchoolEditor extends SchoolBlock {
 			}
 		}
 		
-		ListItem handicapItem = new ListItem();
-		label = new Label(localize("has_handicap", "Has handicap facilities"), hasHandicap);
-		handicapItem.add(hasHandicap);
-		handicapItem.add(label);
-		list.add(handicapItem);
-		section.add(list);
+//		ListItem handicapItem = new ListItem();
+//		label = new Label(localize("has_handicap", "Has handicap facilities"), hasHandicap);
+//		handicapItem.add(hasHandicap);
+//		handicapItem.add(label);
+//		list.add(handicapItem);
+//		section.add(list);
 
 		Layer buttonLayer = new Layer(Layer.DIV);
 		buttonLayer.setStyleClass("buttonLayer");
