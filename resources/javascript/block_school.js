73ce6dda4fa27d2d4c72b94ca151c4cdfe28151a
Valdas Zemaitis <valdas@idega.com>
2008-10-02 08:07:46 +0000

    *** empty log message ***

--- resources/javascript/block_school.js
+++ resources/javascript/block_school.js
@@ -1,11 +1,15 @@
 function initializeSchoolDoubleDropdowns() {
 	$ES("select.schoolTypeDropdown").each(function(item) {
-		item.addEvent('change', function(e){
-			SchoolBusiness.findAllSchoolsByTypeDWR(e.target.value, {
+		item.addEvent('change', function(e) {
+			if (item.id == null) {
+				return false;
+			}
+			
+			SchoolBusiness.findAllSchoolsByTypeDWR(dwr.util.getValue(item.id), {
 				callback: function(result) {
-					var index = e.target.id.lastIndexOf('_');
+					var index = item.id.lastIndexOf('_');
 					if(index > 0) {
-						index = e.target.id.substring(index + 1);
+						index = item.id.substring(index + 1);
 						dwr.util.removeAllOptions($('prm_school_' + index));
 						dwr.util.addOptions($('prm_school_' + index), result, 'id', 'value');
 					}
@@ -15,12 +19,16 @@ function initializeSchoolDoubleDropdowns() {
 		});
 	});
 	$ES("select.schoolDropdown").each(function(item) {
-		item.addEvent('change', function(e){
-			SchoolBusiness.findAllGroupsBySchoolDWR(e.target.value, {
+		item.addEvent('change', function(e) {
+			if (item.id == null) {
+				return false;
+			}
+			
+			SchoolBusiness.findAllGroupsBySchoolDWR(dwr.util.getValue(item.id), {
 				callback: function(result) {
-					var index = e.target.id.lastIndexOf('_');
+					var index = item.id.lastIndexOf('_');
 					if(index > 0) {
-						index = e.target.id.substring(index + 1);
+						index = item.id.substring(index + 1);
 						dwr.util.removeAllOptions($('prm_school_' + index));
 						dwr.util.addOptions($('prm_school_' + index), result, 'id', 'value');
 					}
